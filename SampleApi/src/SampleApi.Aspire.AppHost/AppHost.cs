00e0b2bd97b46042a4f5378c7f0d31247d340f1e
Cody Skidmore <4048971+codyskidmore@users.noreply.github.com>
2026-05-06 15:11:29 -0400

    Next updates (#18)
    * Added an example for integrating with Entra
    
    * Delete README.docx
    
    * Update CoreDesign.slnx
    
    * Updating Solution and repo information to expand packages beyond Identity
    
    * Updates to add more packages and reorganize the code so the solution expands outside just identity
    
    * Updating repository and project settings
    
    * Updates adding a working sample project demonstrating how to setup and use various CoreDesign Nuget packages.
    
    * Updated the readme to include details about how app settings are shared with each project.
    
    * Updated the documentation to include information on the logging middleware.
    
    * Added tests for the packages.
    
    * Created a new package for Logging
    
    * Got the sample working with the logging package
    
    * Update README.md
    
    * Adding santization for logging
    
    * More documenation
    
    * Update CoreDesign.Logging.csproj
    
    * Did some renaming to hopefully make the attributes more readable.
    
    * Update CoreDesign.Logging.csproj
    
    * Reorganizing the code
    
    * Fixed some configuration issues.
    
    * Cleaned up AppHost to make it more readable.
    
    ---------
    
    Co-authored-by: Cody Skidmore <codyskidmore@users.noreply.github.com>

--- SampleApi/src/SampleApi.Aspire.AppHost/AppHost.cs
+++ SampleApi/src/SampleApi.Aspire.AppHost/AppHost.cs
@@ -1,34 +1,15 @@
 using CoreDesign.Shared.Infrastructure;
-using Microsoft.Extensions.Configuration;
-using Projects;
+using SampleApi.Aspire.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddDockerComposeEnvironment("docker-compose");
-
 builder.AddAppSettings();
 
-var dbOptions = builder.Configuration.GetSection(nameof(DatabaseOptions))
-    .Get<DatabaseOptions>();
+var database = builder.AddSqlDatabase();
 
-var sqlPassword = builder.AddParameter("SqlPassword", true);
-
-var dbServer = builder.AddSqlServer(dbOptions.HostName, password: sqlPassword, port: dbOptions.HostPort)
-    .WithDataVolume("sampleapidb-data")
-    .WithImageTag("latest")
-    .WithLifetime(ContainerLifetime.Persistent)
-    .WithImagePullPolicy(ImagePullPolicy.Always);
-
-var database = dbServer.AddDatabase(dbOptions.DatabaseName);
-
-builder.AddProject<SampleApi_Identity_Api>("SampleApiIdentityApi")
-    .WithUrlForEndpoint("https", u => u.DisplayText = "SampleApi Identity (Dev)");
-
-builder.AddProject<SampleApi_Api>("SampleApiApi")
-    .WithUrlForEndpoint("https", u => u.DisplayText = "SampleApi API")
-    .WithReference(database);
-
-builder.AddProject<SampleApi_Data_MigrationService>("SampleApiMigrations")
-    .WithReference(database);
+builder.AddIdentityApi();
+builder.AddSampleApi(database);
+builder.AddMigrationService(database);
 
 builder.Build().Run();
