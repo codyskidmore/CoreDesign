d685616ed74809a271bbee41a2a6f947e173bdfe
Cody Skidmore <codyskidmore@users.noreply.github.com>
2026-05-07 07:19:34 -0400

    Update ClientRecord.cs

--- src/CoreDesign.Identity/CoreDesign.Identity.Server/Clients/ClientRecord.cs
+++ src/CoreDesign.Identity/CoreDesign.Identity.Server/Clients/ClientRecord.cs
@@ -1,6 +1,6 @@
 namespace CoreDesign.Identity.Server.Clients;
 
-public sealed class ClientRecord
+public sealed record ClientRecord
 {
     public string ClientId { get; init; } = string.Empty;
     public string? ClientSecret { get; init; }
